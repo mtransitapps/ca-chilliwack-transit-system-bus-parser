4b6bf4fba73cbe3a09e2ec5c0c53023b4c1ff474
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-15 02:13:25 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
@@ -52,6 +52,11 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		System.out.printf("\nGenerating Chilliwack Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
+	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
 	private static final String INCLUDE_ONLY_SERVICE_ID_CONTAINS = "CW";
 	private static final String INCLUDE_ONLY_SERVICE_ID_CONTAINS2 = "CHW";
 
@@ -183,7 +188,7 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
 	}
@@ -199,7 +204,7 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
 	}

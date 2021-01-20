c9226fd9017f8aa757fef5a9c948d18e090c35d9
Mathieu Méa <mathieu.mea@gmail.com>
2021-01-20 08:17:07 -0500

    GTFS Direction Finder > enable

--- src/main/java/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
@@ -5,28 +5,18 @@ import org.jetbrains.annotations.Nullable;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.StringUtils;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
-import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -156,89 +146,8 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return super.getRouteColor(gRoute);
 	}
 
-	private static final String EXCHANGE_SHORT = "Exch";
-
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-
-	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		//noinspection deprecation
-		map2.put(6L, new RouteTripSpec(6L, //
-				0, MTrip.HEADSIGN_TYPE_STRING, "Chilliwack", //
-				1, MTrip.HEADSIGN_TYPE_STRING, "Cultus Lk") //
-				.addTripSort(0, //
-						Arrays.asList(//
-								"108248", // Sunnyside at Spruce (NB) #CultusLk
-								"108249", //
-								"108190" // Promontory at Vedder (WB) #Chilliwack
-						)) //
-				.addTripSort(1, //
-						Arrays.asList(//
-								"108190", // Promontory at Vedder (WB) #Chilliwack
-								"108085", // ++
-								"108248" // Sunnyside at Spruce (NB) #CultusLk
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(9L, new RouteTripSpec(9L, //
-				0, MTrip.HEADSIGN_TYPE_STRING, "West", //
-				1, MTrip.HEADSIGN_TYPE_STRING, "East") // Downtown
-				.addTripSort(0, //
-						Arrays.asList(//
-								"108253", // Downtown Exchange Bay A
-								"108276", // !=
-								"108113", // xx Cottonwood Mall (NB)
-								"124042", // !=
-								"108161", // !=
-								"108114", // !=
-								"108113" // xx Cottonwood Mall (NB)
-						)) //
-				.addTripSort(1, //
-						Arrays.asList(//
-								"108113", // xx Cottonwood Mall (NB)
-								"124042", // !=
-								"108161", // !=
-								"108114", // xx
-								"108113", // xx Cottonwood Mall (NB)
-								"108114", // xx
-								"108277", // !=
-								"108253" // Downtown Exchange Bay A
-						)) //
-				.compileBothTripSort());
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
-	@Override
-	public int compareEarly(long routeId, @NotNull List<MTripStop> list1, @NotNull List<MTripStop> list2, @NotNull MTripStop ts1, @NotNull MTripStop ts2, @NotNull GStop ts1GStop, @NotNull GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@NotNull
-	@Override
-	public ArrayList<MTrip> splitTrip(@NotNull MRoute mRoute, @Nullable GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@NotNull
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(@NotNull MRoute mRoute, @NotNull GTrip gTrip, @NotNull GTripStop gTripStop, @NotNull ArrayList<MTrip> splitTrips, @NotNull GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
 	@Override
 	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
 		mTrip.setHeadsignString(
 				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
 				gTrip.getDirectionIdOrDefault()
@@ -246,109 +155,25 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	@Override
 	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
-		if (mTrip.getRouteId() == 1L) {
-			if (Arrays.asList( //
-					"Vedder", //
-					"Ufv" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Ufv", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 2L) {
-			if (Arrays.asList( //
-					"Chilliwack", //
-					"Downtown" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 3L) {
-			if (Arrays.asList( //
-					"Downtown", //
-					"Chilliwack" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Chilliwack", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 4L) {
-			if (Arrays.asList( //
-					"Mall", // <>
-					"Malls" // <>
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Malls", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"Mall", // <>
-					"Malls", // <>
-					"Promontory", //
-					"Teskey / Sylvan" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Teskey / Sylvan", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 7L) {
-			if (Arrays.asList( //
-					"Chilliwack", //
-					"Downtown" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"Mall", //
-					"Malls" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Malls", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 8L) {
-			if (Arrays.asList( //
-					"Tyson", //
-					"UFV / Mall" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("UFV / Mall", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 11L) {
-			if (Arrays.asList( //
-					"Agassiz", //
-					"Harrison Hot Spgs", //
-					"Harrison" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Harrison", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 22L) {
-			if (Arrays.asList( //
-					"South", //
-					"Agassiz" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Agassiz", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"North", //
-					"Hope" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Hope", mTrip.getHeadsignId());
-				return true;
-			}
-		}
 		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 	}
 
-	private static final Pattern EXCHANGE = Pattern.compile("(exchange)", Pattern.CASE_INSENSITIVE);
-	private static final String EXCHANGE_REPLACEMENT = EXCHANGE_SHORT;
+	private static final String EXCHANGE_SHORT = "Exch";
+	private static final Pattern EXCHANGE = CleanUtils.cleanWords("exchange");
+	private static final String EXCHANGE_REPLACEMENT = CleanUtils.cleanWordsReplacement(EXCHANGE_SHORT);
 
 	private static final Pattern STARTS_WITH_RSN = Pattern.compile("(^[\\d]+[\\S]+)", Pattern.CASE_INSENSITIVE);
 
 	@NotNull
 	@Override
 	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
-		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign);
+		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign, getIgnoredWords());
 		tripHeadsign = EXCHANGE.matcher(tripHeadsign).replaceAll(EXCHANGE_REPLACEMENT);
 		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
 		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
@@ -356,6 +181,10 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		tripHeadsign = CleanUtils.cleanSlashes(tripHeadsign);
 		tripHeadsign = CleanUtils.cleanStreetTypes(tripHeadsign);
 		return CleanUtils.cleanLabel(tripHeadsign);
+	}
+
+	private String[] getIgnoredWords() {
+		return new String[]{"AM", "PM"};
 	}
 
 	private static final Pattern STARTS_WITH_DCOM = Pattern.compile("(^(\\(-DCOM-\\)))", Pattern.CASE_INSENSITIVE);

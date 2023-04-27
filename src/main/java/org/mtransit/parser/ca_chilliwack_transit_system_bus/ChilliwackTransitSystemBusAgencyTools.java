e4769abdfaaca7719c12d10547e057006e604ff2
Mathieu Méa <mathieu.mea@gmail.com>
2023-04-26 20:08:19 -0400

    Compatibility with latest update

--- src/main/java/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_chilliwack_transit_system_bus/ChilliwackTransitSystemBusAgencyTools.java
@@ -5,7 +5,6 @@ import static org.mtransit.commons.StringUtils.EMPTY;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
-import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
 import org.mtransit.parser.gtfs.data.GRoute;
@@ -15,7 +14,6 @@ import java.util.Locale;
 import java.util.regex.Pattern;
 
 // https://www.bctransit.com/open-data
-// https://www.bctransit.com/data/gtfs/chilliwack.zip
 public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(@NotNull String[] args) {
@@ -27,18 +25,22 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
+	@Override
+	public boolean excludeRoute(@NotNull GRoute gRoute) {
+		if (gRoute.getRouteLongNameOrDefault().contains("FVX")) {
+			return EXCLUDE; // available in Fraser Valley Express app
+		}
+		final int rsn = Integer.parseInt(gRoute.getRouteShortName());
+		if (rsn < 50) {
+			return EXCLUDE; // available in Central Fraser Valley app
+		}
+		return super.excludeRoute(gRoute);
+	}
+
 	@NotNull
 	@Override
 	public String getAgencyName() {
 		return "Chilliwack TS";
-	}
-
-	private static final String AGENCY_ID = "19"; // Chilliwack Transit System only
-
-	@Nullable
-	@Override
-	public String getAgencyId() {
-		return AGENCY_ID;
 	}
 
 	@NotNull
@@ -85,35 +87,34 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	@Nullable
 	@Override
 	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
-		switch (gRoute.getRouteShortName()) {
-		// @formatter:off
-		case "1": return "E21735";
-		case "2": return "7FB539";
-		case "3": return "004A8F";
-		case "4": return "F78B1F";
-		case "5": return "52A6ED";
-		case "6": return "7C3F24";
-		case "7": return "A3238E";
-		case "8": return "49176D";
-		case "9": return "4F6F19";
-		case "11": return "FCAF17";
-		case "22": return "009C3B";
-		case "51": return "E51535";
-		case "52": return "80CC28";
-		case "53": return "61849C";
-		case "54": return "F68712";
-		case "55": return "00ADEF";
-		case "56": return "75321E";
-		case "57": return "A32B9B";
-		case "58": return "401A64";
-		case "59": return "49690F";
-		case "66": return AGENCY_COLOR_BLUE;
-		case "71": return "FBBD09";
-		case "72": return "11AB4A";
-		// @formatter:on
-		default:
-			throw new MTLog.Fatal("Unexpected missing route color for %s!", gRoute);
-		}
+		return switch (gRoute.getRouteShortName()) {
+			// @formatter:off
+			case "1"->"E21735";
+			case "2"->"7FB539";
+			case "3"->"004A8F";
+			case "4"->"F78B1F";
+			case "5"->"52A6ED";
+			case "6"->"7C3F24";
+			case "7"->"A3238E";
+			case "8"->"49176D";
+			case "9"->"4F6F19";
+			case "11"->"FCAF17";
+			case "22"->"009C3B";
+			case "51"->"E51535";
+			case "52"->"80CC28";
+			case "53"->"61849C";
+			case "54"->"F68712";
+			case "55"->"00ADEF";
+			case "56"->"75321E";
+			case "57"->"A32B9B";
+			case "58"->"401A64";
+			case "59"->"49690F";
+			case "66"->AGENCY_COLOR_BLUE;
+			case "71"->"FBBD09";
+			case "72"->"11AB4A";
+			// @formatter:on
+			default -> throw new MTLog.Fatal("Unexpected missing route color for %s!", gRoute);
+		};
 	}
 
 	@Override
@@ -121,7 +122,7 @@ public class ChilliwackTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
-	private static final Pattern STARTS_WITH_RSN = Pattern.compile("(^[\\d]+[\\S]+)", Pattern.CASE_INSENSITIVE);
+	private static final Pattern STARTS_WITH_RSN = Pattern.compile("(^\\d+\\S+)", Pattern.CASE_INSENSITIVE);
 
 	@NotNull
 	@Override
